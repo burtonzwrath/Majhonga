a6d605d6fccd7c746fde7596bfd969b40c49216e
burtonzwrath <burtonzwrath@gmail.com>
2022-10-23 16:49:52 +0300

    rules

---
+++ src/components/Rules.jsx
@@ -0,0 +1,11 @@
+
+function Rules () {
+    return (
+        <div style={{position:"absolute", margin:"0 auto", top:"10px" ,}}>
+          <span>Rules:   </span>
+            <br/>
+            <span>  You should remember cards and find matching pairs</span>
+        </div>
+    )
+}
+export default Rules
